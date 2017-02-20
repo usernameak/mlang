b09a2621af781e96de1bdd87e430618c66127820
UsernameAK <sitemakers.3dn.ru@gmail.com>
2017-02-20 13:31:23 +0200

    added -v parameter to show run time at end

--- interpreter/main.cpp
+++ interpreter/main.cpp
@@ -1,12 +1,33 @@
 #include <fstream>
 #include <iostream>
+#include <chrono>
+#include <cstring>
 #include "runtime.h"
 int main(int argc, char **argv) {
-	if(argc != 2) {
-		std::cout << std::flush << "usage: mlang <compiled source name>" << std::endl;
+	if(argc < 2 || argc > 3) {
+		std::cout << std::flush << "usage: mlang [-v] <compiled source name>" << std::endl;
 		return 1;
 	}
-	std::ifstream* cfile = new std::ifstream(argv[1]);
+	bool verbose = false;
+	if(argc == 3) {
+		if(!strcmp(argv[1], "-v")) {
+			verbose = true;
+		} else {
+			std::cout << std::flush << "usage: mlang [-v] <compiled source name>" << std::endl;
+			return 1;
+		}
+	}
+	char *filename = argv[argc == 3 ? 2 : 1];
+	std::ifstream* cfile = new std::ifstream(filename);
 	Runtime* rt = new Runtime(cfile);
+	std::chrono::steady_clock::time_point begin;
+	if(verbose) {
+		begin = std::chrono::steady_clock::now();
+	}
 	rt->run();
+	if(verbose) {
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		std::cout << "Run time:" << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << std::endl;
+	}
+	
 }
